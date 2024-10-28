5f3ddef3c21637526e07a628f49788a5581487ed
Gareth Park <garethpark@yahoo.com>
2024-10-28 17:48:02 +0000

    add items

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 export default function Home() {
   return (
     <div className="flex bg-gray-100 p-4">
-      Hello World
+      <div className="flex-1 bg-red-500 p-4 text-white">Item 1</div>
+      <div className="flex-1 bg-blue-500 p-4 text-white">Item 2</div>
+      <div className="flex-1 bg-green-500 p-4 text-white">Item 3</div>
     </div>
   );
 }
