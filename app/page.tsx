7d7d0effd57449f4b33b285582d15a922b41633f
Gareth Park <garethpark@yahoo.com>
2024-10-29 14:22:32 +0000

    correct flex

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 
 export default function Home() {
   return (
-    <div className="flex bg-gray-100 p-4 space-x-4">
-      <div className="flex-1 bg-red-500 p-4 text-white">Item 1</div>
-      <div className="flex-1 bg-blue-500 p-4 text-white">Item 2</div>
-      <div className="flex-1 bg-green-500 p-4 text-white">Item 3</div>
+    <div className="flex justify-between items-center p-4 bg-gray-100">
+      <div className="bg-red-500 p-4 text-white">Item 1</div>
+      <div className="bg-blue-500 p-4 text-white">Item 2</div>
+      <div className="bg-green-500 p-4 text-white">Item 3</div>
     </div>
   );
 }
