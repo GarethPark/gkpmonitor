0d7c9bc00d29964d640f752199201c25e4db730e
Gareth Park <garethpark@yahoo.com>
2024-10-29 14:45:12 +0000

    flex no wrap

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-wrap bg-gray-100 p-4 space-x-4">
+    <div className="flex flex-wrap-nowrap bg-gray-100 p-4 space-x-4">
       <div className="bg-red-500 p-4 text-white">Item 1</div>
       <div className="bg-blue-500 p-4 text-white">Item 2</div>
       <div className="bg-green-500 p-4 text-white">Item 3</div>
