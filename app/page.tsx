5f15df58426f75588185ddbd14928dae2365cae4
Gareth Park <garethpark@yahoo.com>
2024-10-29 14:51:10 +0000

    flex grow

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-wrap-nowrap bg-gray-100 p-4 space-x-4">
-      <div className="bg-red-500 p-4 text-white">Item 1</div>
+    <div className="flex bg-gray-100 p-4">
+      <div className="bg-red-500 p-4 text-white flex-grow">Item 1</div>
       <div className="bg-blue-500 p-4 text-white">Item 2</div>
-      <div className="bg-green-500 p-4 text-white">Item 3</div>
+      <div className="bg-green-500 p-4 text-white flex-grow">Item 3</div>
       <div className="bg-yellow-500 p-4 text-white">Item 4</div>
-      <div className="bg-purple-500 p-4 text-white">Item 5</div>
+      <div className="bg-purple-500 p-4 text-white flex-grow">Item 5</div>
     </div>
   );
 }
