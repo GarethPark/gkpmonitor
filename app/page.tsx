ba4e43a032dfd6dfb57773440dd6a8023535914a
Gareth Park <garethpark@yahoo.com>
2024-10-29 14:55:17 +0000

    change flex to grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 
 export default function Home() {
   return (
-    <div className="flex bg-gray-100 p-4">
-      <div className="bg-red-500 p-4 text-white flex-grow">Item 1</div>
+    <div className="grid grid-cols-3 gap-4 p-4 bg-gray-100">
+      <div className="bg-red-500 p-4 text-white">Item 1</div>
       <div className="bg-blue-500 p-4 text-white">Item 2</div>
-      <div className="bg-green-500 p-4 text-white flex-grow">Item 3</div>
+      <div className="bg-green-500 p-4 text-white">Item 3</div>
       <div className="bg-yellow-500 p-4 text-white">Item 4</div>
-      <div className="bg-purple-500 p-4 text-white flex-grow">Item 5</div>
+      <div className="bg-purple-500 p-4 text-white">Item 5</div>
+      <div className="bg-pink-500 p-4 text-white">Item 6</div>
     </div>
   );
 }
