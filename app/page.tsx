0b1036272cc69b8bd1145b079037c333a7836e5a
Gareth Park <garethpark@yahoo.com>
2024-10-29 14:40:39 +0000

    items right

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-start bg-gray-100 space-y-12">
+    <div className="flex flex-col items-end bg-gray-100 space-y-12">
       <div className="bg-red-500 p-4 text-white">Item 1</div>
       <div className="bg-blue-500 p-4 text-white">Item 2</div>
       <div className="bg-green-500 p-4 text-white">Item 3</div>
