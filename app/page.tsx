30dea2a9b149213ac9de83b16f3965845bc108cc
Gareth Park <garethpark@yahoo.com>
2024-10-29 14:58:06 +0000

    adjust column spans

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@
 export default function Home() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 bg-gray-100">
-      <div className="bg-red-500 p-4 text-white">Item 1</div>
+      <div className="bg-red-500 p-4 text-white col-span-2">Item 1</div>
       <div className="bg-blue-500 p-4 text-white">Item 2</div>
       <div className="bg-green-500 p-4 text-white">Item 3</div>
       <div className="bg-yellow-500 p-4 text-white">Item 4</div>
       <div className="bg-purple-500 p-4 text-white">Item 5</div>
-      <div className="bg-pink-500 p-4 text-white">Item 6</div>
+      <div className="bg-pink-500 p-4 text-white col-span-2">Item 6</div>
     </div>
   );
 }
