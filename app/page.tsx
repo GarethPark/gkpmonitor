824610072cd7c43b47f284f6ee20dd13a93210d6
Gareth Park <garethpark@yahoo.com>
2024-10-29 14:26:00 +0000

    add spacing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
 export default function Home() {
   return (
-    <div className="flex justify-between items-center p-4 bg-gray-100">
+    <div className="flex bg-gray-100 space-x-8">
       <div className="bg-red-500 p-4 text-white">Item 1</div>
       <div className="bg-blue-500 p-4 text-white">Item 2</div>
       <div className="bg-green-500 p-4 text-white">Item 3</div>
